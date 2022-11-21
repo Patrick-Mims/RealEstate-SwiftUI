2614849fe05175b1bbc43c65ca1add2473718cd3
Patrick Mims <patrickkevinmims@gmail.com>
2022-11-20 18:44:41 -0800

    Initial Commit

---
+++ RealEstate/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RealEstate
+//
+//  Created by JPL-ST-SPRING2022 on 11/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RealEstate/RealEstateApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealEstateApp.swift
+//  RealEstate
+//
+//  Created by JPL-ST-SPRING2022 on 11/20/22.
+//
+
+import SwiftUI
+
+@main
+struct RealEstateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
